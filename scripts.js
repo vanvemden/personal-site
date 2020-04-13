f19c1a11a1e9ca323375a7d1fe29a58f3b030545
Marco van Vemden <vanvemden@gmail.com>
2020-04-12 22:32:18 -0700

    added images and content

--- scripts.js
+++ scripts.js
@@ -5,7 +5,7 @@ window.addEventListener('load', (event) => {
        let icons = document.querySelectorAll(".overlay .glyphicon");
         for (let i = 0; i < icons.length; i++) {
             icons[i].addEventListener("click", function(event) {
-                event.target.parentElement.classList.toggle("overlay");
+                event.target.parentElement.parentElement.classList.toggle("overlay");
             })
         }
 
