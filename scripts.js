3339f577b701d9ef004b08dd02c824124c1e1632
Marco van Vemden <vanvemden@gmail.com>
2020-08-20 16:26:41 -0700

    update toggle function

--- scripts.js
+++ scripts.js
@@ -1,14 +1,14 @@
 window.addEventListener('load', (event) => {
 
-   (function () {
-       // toggle no or show text icons
-       let icons = document.querySelectorAll(".overlay .glyphicon");
+    (function () {
+        // toggle no or show text icons
+        let icons = document.querySelectorAll(".overlay .toggle");
         for (let i = 0; i < icons.length; i++) {
-            icons[i].addEventListener("click", function(event) {
+            icons[i].addEventListener("click", function (event) {
                 event.target.parentElement.parentElement.classList.toggle("overlay");
             })
         }
 
-   })()
+    })()
 
 });
