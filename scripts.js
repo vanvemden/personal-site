dbeb551342d364cabf082aebc11d876ea98de10d
Marco van Vemden <vanvemden@gmail.com>
2020-08-23 10:12:01 -0700

    collapse inactive project text on smaller screens

--- scripts.js
+++ scripts.js
@@ -21,10 +21,13 @@ window.addEventListener("load", (event) => {
       button.addEventListener("click", function (event) {
         let project_num = event.target.dataset.project;
 
-        // Set currently active project button to inactive
+        // Set currently active project to inactive
         document.querySelectorAll(".skills .btn-success").forEach((e) => {
+          // change button style to outline
           e.classList.remove("btn-success");
           e.classList.add("btn-outline-success");
+          // hide project text on smaller screens
+          e.parentElement.parentElement.children[1].classList.add("d-none");
         });
 
         // Set currently active badges to inactive
@@ -39,6 +42,10 @@ window.addEventListener("load", (event) => {
           .forEach((e) => {
             e.classList.add("btn-success");
             e.classList.remove("btn-outline-success");
+            // Show project text on smaller screens
+            e.parentElement.parentElement.children[1].classList.remove(
+              "d-none"
+            );
           });
 
         // Set active badges for selected project number
@@ -54,8 +61,13 @@ window.addEventListener("load", (event) => {
     /**
      * By default, set project 1 button and skill badges to active
      */
-    buttons[0].classList.toggle("btn-success");
-    buttons[0].classList.toggle("btn-outline-success");
+    buttons[0].classList.add("btn-success");
+    buttons[0].classList.remove("btn-outline-success");
+    // Show project text on smaller screens
+    buttons[0].parentElement.parentElement.children[1].classList.remove(
+      "d-none"
+    );
+    // Set active badges for project 1
     document.querySelectorAll(".skills .badge.proj-1").forEach((e) => {
       e.style.color = inactiveBadgeColor;
       e.style.backgroundColor = activeBadgeColor;
